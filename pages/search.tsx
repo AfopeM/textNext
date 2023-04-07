090418a380682c97acef9dcad179935e8af7ccab
Oluwatobi <afopematilukuro@gmail.com>
2023-04-07 11:38:11 -0400

    commit 1

---
+++ pages/search.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Search() {
+  return (
+    <>
+      <div className="text-4xl uppercase text-blue-700">Search</div>
+      <Link href="/">Go Back</Link>
+    </>
+  );
+}
